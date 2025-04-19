7fe096016d42d574c69f557c992d82febf14116d
evelioexcellenta <velioexcel@gmail.com>
2025-04-19 10:36:38 +0700

    create server folder

---
+++ server/src/main/java/id/co/bsi/Vuluz/VuluzApplication.java
@@ -0,0 +1,13 @@
+package id.co.bsi.Vuluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuluzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuluzApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/id/co/bsi/Vuluz/VuluzApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bsi.Vuluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuluzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
